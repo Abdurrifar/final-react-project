d93737e99c9cc7f31fe05105b6a8135ec6000837
abdur refat <abdurrifat2022@gmail.com>
2026-05-30 23:38:04 +0600

    daisy ui

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+
+
+// const footer = () => {
+//     return (
+//         <div>
+//             <h1>hello ami footer</h1>
+//         </div>
+//     );
+// };
+
+// export default footer;
---
+++ src/components/header/navbar.jsx
@@ -0,0 +1,11 @@
+
+
+// const navbar = () => {
+//     return (
+//         <div>
+//             <h1>hello ami navbar</h1>
+//         </div>
+//     );
+// };
+
+// export default navbar;
