acf102bb1fbd54e17924d05fce0bfa010cb6ace0
abdur refat <abdurrifat2022@gmail.com>
2026-06-13 01:29:21 +0600

    sweetAlert and react toast

--- src/pages/Bookdetails/Bookdetails.jsx
+++ src/pages/Bookdetails/Bookdetails.jsx
@@ -1,5 +1,10 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+const MySwal = withReactContent(Swal)
+import { ToastContainer, toast } from 'react-toastify';
 import { useLoaderData, useParams } from "react-router";
 import { addTOStroedDB } from "../../utilities/addToDB";
+
 
 
 const Bookdetails = () => {
@@ -14,6 +19,16 @@ const Bookdetails = () => {
 
     const handleMarkAsRead = (id) => {
 
+        // MySwal.fire({
+        //     title: <p>Hello World</p>,
+        //     didOpen: () => {
+
+        //         MySwal.showLoading()
+        //     },
+        // }).then(() => {
+        //     return MySwal.fire(<p>Shorthand works too</p>)
+        // })
+        toast("Wow so easy!")
         addTOStroedDB(id)
     }
 
@@ -22,7 +37,7 @@ const Bookdetails = () => {
         <div>
             <img className="w-48" src={image} alt="" />
             <h2>{bookName}</h2>
-
+            <ToastContainer />
             <button onClick={() => handleMarkAsRead(id)} className="btn btn-accent m-2">As a Read</button>
             <button className="btn btn-info m-2">WishList</button>
         </div>
