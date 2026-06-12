a83178009585c92c447faceada83086782ccbf85
abdur refat <abdurrifat2022@gmail.com>
2026-06-12 18:22:00 +0600

    already read for localStorage

--- src/pages/Bookdetails/Bookdetails.jsx
+++ src/pages/Bookdetails/Bookdetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router";
+import { addTOStroedDB } from "../../utilities/addToDB";
 
 
 const Bookdetails = () => {
@@ -7,13 +8,26 @@ const Bookdetails = () => {
     const parseId = parseInt(id)
     const data = useLoaderData()
     const singlebook = data.find(book => book.bookId === parseId)
-    const { bookName, image } = singlebook
+    const { bookName, image } = singlebook || {}
 
-    console.log(id);
+
+
+    // upor ar toku hocca click a dakhar kaj ar nicer gula and addToDB component hocca button ar kaj
+
+
+    const handleMarkAsRead = (id) => {
+
+        addTOStroedDB(id)
+    }
+
+
     return (
         <div>
             <img className="w-48" src={image} alt="" />
             <h2>{bookName}</h2>
+
+            <button onClick={() => handleMarkAsRead(id)} className="btn btn-accent m-2">As a Read</button>
+            <button className="btn btn-info m-2">WishList</button>
         </div>
     );
 };
---
+++ src/utilities/addToDB.js
@@ -0,0 +1,27 @@
+const getStoreBook = () => {
+
+    const storeBookSTR = localStorage.getItem('readList')
+    if (storeBookSTR) {
+        const storeBookData = JSON.parse(storeBookSTR)
+        return storeBookData
+    }
+    else { return [] }
+
+}
+
+const addTOStroedDB = (id) => {
+    const storeBookData = getStoreBook()
+
+    if (storeBookData.includes(id)) {
+        console.log('helo')
+        alert('vi ai id already aca ')
+    }
+    else {
+        storeBookData.push(id)
+        const data = JSON.stringify(storeBookData)
+        localStorage.setItem("readList", data)
+        console.log(storeBookData)
+    }
+
+}
+export { addTOStroedDB }
